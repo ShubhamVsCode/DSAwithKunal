de5181817cd5b1ffbb7c3e13d2dee886a77e033b
ShubhamVsCode <shubhamvscode@gmail.com>
2022-05-28 13:12:03 +0530

    Question 2 completed

---
+++ Questions/AssignmentsByKunal/Q2.java
@@ -0,0 +1,21 @@
+package Questions.AssignmentsByKunal;
+
+public class Q2 {
+    // Define a program to find out whether a given number is even or odd.
+    public static void main(String[] args) {
+
+        int n = -45;
+        evenOrOdd(n);
+    }
+
+    static void evenOrOdd(int n){
+        if (n%2 == 0){
+            System.out.println("Even");
+        } else{
+            System.out.println("Odd");
+        }
+    }
+
+
+
+}
